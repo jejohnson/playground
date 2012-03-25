be17c0cee0bcd68f914fc53b9da4cd26ee991b44
= <=>
2012-03-24 20:15:34 -0700

    Trying out git from intellij

---
+++ src/info/jejohnson/play/HelloWorld.java
@@ -0,0 +1,14 @@
+package info.jejohnson.play;
+
+/**
+ * User: jamesjohnson
+ * Date: 3/24/12
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+    
+}
